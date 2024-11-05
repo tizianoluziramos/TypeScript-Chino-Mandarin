8dfb90ffae061cd515236de56b9f0dc183db22cf
tizianoluziramos <github@perfect.wuaze.com>
2024-11-04 21:53:50 -0300

    Primer commit

---
+++ TypeScript/Terminados/src/importar.ts
@@ -0,0 +1,124 @@
+import { 当条件成立 } from './while.ts';
+import { 尝试捕获 } from './try.ts';
+import { 案例 } from './switch.ts';
+import { 定义超时 } from './setTimeout.ts';
+import rls工具 from './rlsUtils.ts';
+import { 返回 } from './return.ts';
+import { 执行器 } from './Promise.ts';
+import { 循环 } from './for.ts';
+import { 查找对象 } from './find.ts';
+import { 定义变量, 变量 } from './const.ts';
+import { 控制台 } from './console.ts';
+import { 字符在 } from './charAt.ts';
+import { 
+    映射, 
+    过滤, 
+    减少, 
+    找到, 
+    找到索引, 
+    包含, 
+    合并, 
+    分割, 
+    排序, 
+    反转, 
+    连接, 
+    推入, 
+    删除, 
+    切割, 
+    每个 
+} from './arrayUtil.ts';
+import { 如果 } from './IF/helper.ts';
+import 流在中文 from './Extras/stream-web-es.ts';
+import 操作系统, { 获取系统信息 } from './Extras/os-es.ts';
+import momentEs from './Extras/moment-es.ts';
+import lodashEs from './Extras/lodash-es.ts';
+import { TypeScript的中文致谢 } from './Extras/Creditos.ts';
+import axiosEs from './Extras/axios-es.ts';
+import { 验证类型 } from './AddOns/validarTipo.ts';
+import { 执行Python脚本 } from './AddOns/RunPython.ts';
+import { fsEs } from './AddOns/fsEs.ts';
+import { 控制台日志 } from './AddOns/Consologuear.ts';
+import { 验证卡号 } from './AddOns/Others/validarTarjeta.ts';
+import { 敏捷开发测试 } from './AddOns/Others/pruebaDeScrum.ts';
+import { 反转字符串 } from './AddOns/Others/InvertirCadena.ts';
+import { 反转数组 } from './AddOns/Others/invertirarray.ts';
+import { 生成二维码 } from './AddOns/Others/generarQR.ts';
+import { 阶乘 } from './AddOns/Others/Factorial.ts';
+import { 是回文 } from './AddOns/Others/esPalindromo.ts';
+import { 是字谜 } from './AddOns/Others/esAnagrama.ts';
+import { 转换为二进制 } from './AddOns/Others/convertirDeNumeroABinario.ts';
+import { 转换 } from './AddOns/Others/convertidorDeUnidades.ts';
+import { 计算元音数量 } from './AddOns/Others/contarVocales.ts';
+import { 密钥管理器, 登录 } from './AddOns/Others/LoginSystems/login1.ts';
+import { 生成Windows95密钥 } from './AddOns/Others/Activator/Windows95.ts';
+import { 读取内存中的进程 } from './AddOns/Other/leerMemoriaRAM.ts';
+import { 读取Word文件 } from './AddOns/Other/leerArchivoWord.ts';
+import { 读取PDF } from './AddOns/Other/leerArchivoPDF.ts';
+import { 下载文件 } from './AddOns/Other/descargarArchivo.ts';
+import { 创建服务器 } from './AddOns/Other/crearServidor.ts';
+import { 转换EXE为十六进制 } from './AddOns/Other/convertirEXEaHexadecimal.ts';
+import { 连接SFTP } from './AddOns/Other/AccesoFTP.ts';
+
+export {
+    当条件成立,
+    尝试捕获,
+    案例,
+    定义超时,
+    rls工具,
+    返回,
+    执行器,
+    循环,
+    查找对象,
+    定义变量,
+    变量,
+    控制台,
+    字符在,
+    映射,
+    过滤,
+    减少,
+    找到,
+    找到索引,
+    包含,
+    合并,
+    分割,
+    排序,
+    反转,
+    连接,
+    推入,
+    删除,
+    切割,
+    每个,
+    如果,
+    流在中文,
+    操作系统,
+    获取系统信息,
+    momentEs,
+    lodashEs,
+    TypeScript的中文致谢,
+    axiosEs,
+    验证类型,
+    执行Python脚本,
+    fsEs,
+    控制台日志,
+    验证卡号,
+    敏捷开发测试,
+    反转字符串,
+    反转数组,
+    生成二维码,
+    阶乘,
+    是回文,
+    是字谜,
+    转换为二进制,
+    转换,
+    计算元音数量,
+    密钥管理器,
+    登录,
+    生成Windows95密钥,
+    读取内存中的进程,
+    读取Word文件,
+    读取PDF,
+    下载文件,
+    创建服务器,
+    转换EXE为十六进制,
+    连接SFTP
+};
